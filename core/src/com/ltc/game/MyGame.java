74544c8aced3f3d3dc6b51478a5c0075d72060d9
SapryginDanila <saprygindanila@gmail.com>
2017-04-22 08:48:20 +0600

    поменял цвет

--- core/src/com/ltc/game/MyGame.java
+++ core/src/com/ltc/game/MyGame.java
@@ -18,7 +18,8 @@ public class MyGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		//зеленый лучше
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
