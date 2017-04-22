79a24803ca3cc11e10f1a81f6f89efdaaeb69eff
Artem <klimenkocpp@icloud.com>
2017-04-22 22:04:33 +0600

    some changes

--- core/src/com/ltc/game/GameScreen.java
+++ core/src/com/ltc/game/GameScreen.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.ltc.game.entities.BotIdleEntity;
 import com.ltc.game.entities.PlayerProgerEntity;
@@ -14,6 +17,8 @@ import com.ltc.game.entities.PlayerVlogerEntity;
 import com.ltc.game.entities.WallEntiy;
 
 import java.util.ArrayList;
+
+import static java.lang.Math.floor;
 
 /**
  * @author Velkonost
