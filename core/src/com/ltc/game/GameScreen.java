fe468201a9777c555f7288a05b8671da72666901
Artem <klimenkocpp@icloud.com>
2017-04-23 14:53:05 +0600

    some changes

--- core/src/com/ltc/game/GameScreen.java
+++ core/src/com/ltc/game/GameScreen.java
@@ -452,15 +452,13 @@ public class GameScreen extends BaseScreen {
                     e.printStackTrace();
                 }
                 socket.emit("playerMoved", data);
-
             }
         }
-        ;
     }
 
     public void connectSocket(){
         try {
-            socket  = IO.socket("http://192.168.10.124:3000");
+            socket  = IO.socket("http://25.36.204.209:5665");
 //            socket = IO.socket("http://766ee2e4.ngrok.io");
 //            socket = IO.socket("http://94.251.109.165:80");
             socket.connect();
